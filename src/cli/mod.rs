ec050c6cc61d2537bc2631a3f1ddb9d666ea46a1
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-09-22 17:12:43 +0530

    cli: is_build_command() function and build mode check

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,4 +1,20 @@
+use crate::host;
+
 /// Parses and executes command-line arguments.
 pub fn run(args: Vec<String>) {
-    println!("{:#?}", args);
+    if !args.is_empty() && is_build_command(&args[0]) {
+        // Build mode
+        host::writeln!("Build command is not implemented yet!");
+        host::exit(1);
+    } else {
+        // Normal mode
+    }
+}
+
+fn is_build_command(name: &str) -> bool {
+    let name = name.to_lowercase();
+    if name == "-build" || name == "--build" || name == "-b" || name == "--b" {
+        return true;
+    }
+    false
 }
--- src/host/native/mod.rs
+++ src/host/native/mod.rs
@@ -2,3 +2,18 @@
 pub fn args() -> Vec<String> {
     std::env::args().skip(1).collect()
 }
+
+#[macro_export]
+macro_rules! writeln {
+    () => {
+        println!()
+    };
+    ($($arg:tt)*) => {
+        println!($($arg)*)
+    };
+}
+pub use crate::writeln; // Re-export the crate-root macro from native
+
+pub fn exit(code: i32) {
+    std::process::exit(code);
+}
